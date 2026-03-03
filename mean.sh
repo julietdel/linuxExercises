58b00533eecb505eeff3d38b6543b1a7560efafd
ebogenschutz12 <ebogenschutz@wisc.edu>
2026-03-03 08:38:32 -0600

    Update mean.sh

--- mean.sh
+++ mean.sh
@@ -1,1 +1,26 @@
 #!/bin/bash
+usage() {
+    echo "usage: ./mean.sh <column> [file.csv]" >&2
+    exit 1
+}
+
+if [ $# -lt 1 ] || [ $# -gt 2 ]; then
+    usage
+fi
+
+col="$1"
+
+case "$col" in
+    ''|*[!0-9]*)
+        usage
+        ;;
+esac
+
+if [ $# -eq 2 ]; then
+    file="$2"
+else
+    file="/dev/stdin"
+fi
+
+cut -d',' -f"$col" "$file" | tail -n +2 | \
+awk '{ sum += $1; n++ } END { if (n>0) print sum/n; else print 0 }'
