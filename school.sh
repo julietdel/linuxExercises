4bf9b6235496f636b0d7763253a57a01258f54b4
YuHan Luo <yluo298@wisc.edu>
2026-03-03 08:13:20 -0600

    Finish school.sh

--- school.sh
+++ school.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+cat Property_Tax_Roll.csv \
+| grep "MADISON SCHOOLS" \
+| cut -d, -f7 \
+| {
+    sum=0
+    n=0
+
+    while read -r value
+    do
+        sum=$(awk -v s="$sum" -v v="$value" 'BEGIN { print s + v }')
+        n=$((n + 1))
+    done
+
+    awk -v s="$sum" -v n="$n" 'BEGIN { print s / n }'
+}
+	
